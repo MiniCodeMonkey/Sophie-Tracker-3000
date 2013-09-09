4dca0d22fa8a7b52ae9ea445171ea10c385c7d91
Mathias Hansen <me@codemonkey.io>
2013-09-09 19:46:10 -0400

    Added Guard back to production

--- app/config/app.php
+++ app/config/app.php
@@ -112,7 +112,8 @@ return array(
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-		'Illuminate\Workbench\WorkbenchServiceProvider'
+		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Way\Console\GuardLaravelServiceProvider'
 
 	),
 
